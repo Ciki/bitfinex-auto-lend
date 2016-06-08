e8fc2acd7195888a7bcfdd03a5db65716afc589b
Ciki <matula.m@gmail.com>
2016-06-08 12:57:19 +0200

    use absolute paths

--- config-usd.php
+++ config-usd.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 
-include_once('./config.php');
+include_once(__DIR__ . '/config.php');
 
 $config['currency'] = 'USD'; // Currency to lend
 $config['period'] = 2; // Number of days to lend
@@ -8,8 +8,8 @@ $config['minimum_balance'] = 50; // Minimum balance to be able to lend (bitfinex
 $config['remove_after'] = 50; // Minutes after unexecuted offer gets cancelled
 $config['max_total_swaps'] = 20000; // Max number of total swaps to check for a closest rate *
 
-// * If there are e.g. total of 12000 swaps at 1%/day and 22000 
-// swaps at 1.001%/day the script chooses the lower rate, because 
+// * If there are e.g. total of 12000 swaps at 1%/day and 22000
+// swaps at 1.001%/day the script chooses the lower rate, because
 // we don't want to go beyond the rate of 20000 swaps.
 
 ?>
--- lend.php
+++ lend.php
@@ -2,9 +2,9 @@
 
 $currency = @$_GET['currency'] ? htmlspecialchars($_GET['currency']) : 'usd';
 
-include_once("./config-$currency.php"); 
-include_once('./functions.php');
-include_once('./bitfinex.php');
+include_once(__DIR__ . "/config-$currency.php");
+include_once(__DIR__ . '/functions.php');
+include_once(__DIR__ . '/bitfinex.php');
 
 $bfx = new Bitfinex($config['api_key'], $config['api_secret']);
 
@@ -23,7 +23,7 @@ foreach( $current_offers as $item )
 	$timestamp = (int) $item['timestamp'];
 	$current_timestamp = time();
 	$diff_minutes = round(($current_timestamp - $timestamp) / 60);
-	
+
 	if( $config['remove_after'] <= $diff_minutes )
 	{
 		message("Removing offer # $id");
@@ -42,7 +42,7 @@ if( $balances )
 		if( $item['type'] == 'deposit' && $item['currency'] == strtolower($config['currency']) )
 		{
 			$available_balance = floatval($item['available']);
-			
+
 			break;
 		}
 	}
@@ -52,28 +52,28 @@ if( $balances )
 if( $available_balance >= $config['minimum_balance'] )
 {
 	message("Lending availabe balance of $available_balance");
-	
+
 	$lendbook = $bfx->get_lendbook($config['currency']);
 	$offers = $lendbook['asks'];
-	
+
 	$total_amount 	= 0;
 	$next_rate 		= 0;
 	$next_amount 	= 0;
 	$check_next 	= FALSE;
-	
+
 	// Find the right rate
 	foreach( $offers as $item )
-	{	
+	{
 		// Save next closest item
 		if( $check_next )
 		{
-			$next_rate 		= $item['rate'];	
+			$next_rate 		= $item['rate'];
 			$next_amount 	= $item['amount'];
 			$check_next 	= FALSE;
 		}
-		
+
 		$total_amount += floatval($item['amount']);
-	
+
 		// Possible the closest rate to what we want to lend
 		if( $total_amount <= $config['max_total_swaps'] )
 		{
@@ -81,17 +81,17 @@ if( $available_balance >= $config['minimum_balance'] )
 			$check_next = TRUE;
 		}
 	}
-	
+
 	// Current rate is too low, move closer to the next rate
 	if( $next_amount <= $config['max_total_swaps'] )
 	{
 		$rate = $next_rate - 0.01;
 	}
-	
+
 	$daily_rate = daily_rate($rate);
-	
+
 	$result = $bfx->new_offer($config['currency'], (string) $available_balance, (string) $rate, $config['period'], 'lend');
-	
+
 	// Successfully lent
 	if( array_key_exists('id', $result) )
 	{
