e0a2452371e226fb9502cc402bc441e49feb4d8c
Ciki <matula.m@gmail.com>
2016-06-08 16:48:47 +0200

    do not lend all money at once, lend fix amount, vary lending interval

--- lend.php
+++ lend.php
@@ -39,73 +39,93 @@ if ($balances) {
 	foreach ($balances as $item) {
 		if ($item['type'] === 'deposit' && $item['currency'] === strtolower($config['currency'])) {
 			$available_balance = floatval($item['available']);
-
+			$totalBalance = floatval($item['amount']);
 			break;
 		}
 	}
 }
 
+
+// todo: do not lend all money at once
+// lend fix amount, vary lending period
+// https://docs.google.com/document/d/1wpd6r2izpDdkDa3UBBBgUboJCyLTR85dqRhLydcS-xA/edit#heading=h.c8khatrecsgt
+$lendingAmount = $config['minimum_balance'];
+$periodMinutes = $config['period'] * 1440;
+$numberOfLoansPerPeriod = floor($totalBalance / $lendingAmount);
+$lendingInterval = ceil($periodMinutes / $numberOfLoansPerPeriod); // in minutes
+
+// check time since last loan
+$lastLoanTimeFilepath = __DIR__ . '/.last_loan_time';
+$lastLoanTime = file_exists($lastLoanTimeFilepath) ? file_get_contents($lastLoanTimeFilepath) : 0;
+$timeSinceLastLoan = time() - $lastLoanTime;
+//d([$lendingAmount, $lendingInterval, $periodMinutes, $numberOfLoansPerPeriod, $timeSinceLastLoan]);die;
+if ($timeSinceLastLoan < ($lendingInterval * 60)) {
+	message('waiting for lending interval..last loan offer placed before ' . round($timeSinceLastLoan / 60) . ' minutes');
+	exit(0);
+} else {
+
 // Is there enough balance to lend?
-if ($available_balance >= $config['minimum_balance']) {
-	message("Lending availabe balance of $available_balance");
+//	if ($available_balance >= $config['minimum_balance']) {
+	if ($available_balance >= $lendingAmount) {
+		message("Lending availabe balance of $available_balance");
 
-	$lendbook = $bfx->get_lendbook($config['currency']);
+		$lendbook = $bfx->get_lendbook($config['currency']);
 //	d($lendbook, 'lendbook');
-	$offers = $lendbook['asks'];
-	d($offers, 'offers');
+		$offers = $lendbook['asks'];
+		d($offers, 'offers');
 
-	$total_amount = 0;
-	$next_rate = 0;
-	$next_amount = 0;
-	$check_next = FALSE;
+		$total_amount = 0;
+		$next_rate = 0;
+		$next_amount = 0;
+		$check_next = FALSE;
 
-	// Find the right rate
-	foreach ($offers as $item) {
-		// Save next closest item
-		if ($check_next) {
-			$next_rate = $item['rate'];
-			$next_amount = $item['amount'];
-			$check_next = FALSE;
+		// Find the right rate
+		foreach ($offers as $item) {
+			// Save next closest item
+			if ($check_next) {
+				$next_rate = $item['rate'];
+				$next_amount = $item['amount'];
+				$check_next = FALSE;
+			}
+
+			$total_amount += floatval($item['amount']);
+
+			// Possible the closest rate to what we want to lend
+			if ($total_amount <= $config['max_total_swaps']) {
+				$rate = $item['rate'];
+				$check_next = TRUE;
+			}
 		}
 
-		$total_amount += floatval($item['amount']);
-
-		// Possible the closest rate to what we want to lend
-		if ($total_amount <= $config['max_total_swaps']) {
-			$rate = $item['rate'];
-			$check_next = TRUE;
-		}
-	}
-
-	// Current rate is too low, move closer to the next rate
-	// ???
+		// Current rate is too low, move closer to the next rate
+		// ???
 //	if ($next_amount <= $config['max_total_swaps']) {
 //		$rate = $next_rate - 0.01;
 //	}
 
-	$daily_rate = daily_rate($rate);
-	$daily_next_rate = daily_rate($next_rate);
+		$daily_rate = daily_rate($rate);
+		$daily_next_rate = daily_rate($next_rate);
 
-	// if there's a gap between current rate & next rate bigger than one `tick`
-	// make the rate as high as possible below the next_rate
-	if ($daily_next_rate - $daily_rate > 0.0001) {
-		$daily_rate = $daily_next_rate - 0.0001;
-		$rate = $daily_rate * 365;
-	}
-	d([$daily_rate, $rate, $daily_next_rate, $next_rate], 'rates');
-//	die;
+		// if there's a gap between current rate & next rate bigger than one `tick`
+		// make the rate as high as possible below the next_rate
+		if ($daily_next_rate - $daily_rate > 0.0001) {
+			$daily_rate = $daily_next_rate - 0.0001;
+			$rate = $daily_rate * 365;
+		}
+//		d([$daily_rate, $rate, $daily_next_rate, $next_rate], 'rates');
+//		die;
 
-	// todo: do not lend all money at once
-	$lendingAmount = (string) $available_balance;
-	$result = $bfx->new_offer($config['currency'], $lendingAmount, (string) $rate, $config['period'], 'lend');
+		$result = $bfx->new_offer($config['currency'], $lendingAmount, (string) $rate, $config['period'], 'lend');
 
-	// Successfully lent
-	if (array_key_exists('id', $result)) {
-		message("$available_balance {$config['currency']} lent for {$config['period']} days at daily rate of $daily_rate%. Offer id {$result['id']}.");
+		// Successfully lent
+		if (array_key_exists('id', $result)) {
+			message("$lendingAmount {$config['currency']} lent for {$config['period']} days at daily rate of $daily_rate%. Offer id {$result['id']}.");
+			file_put_contents($lastLoanTimeFilepath, time());
+		} else {
+			// Something went wrong
+			message($result);
+		}
 	} else {
-		// Something went wrong
-		message($result);
+		message("Balance of $available_balance {$config['currency']} is not enough to lend.");
 	}
-} else {
-	message("Balance of $available_balance {$config['currency']} is not enough to lend.");
 }
